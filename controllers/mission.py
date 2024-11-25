13aa370e55981dedb562e95d037735d9c1a52172
bloodofspring <t.shagin@gmail.com>
2024-11-25 09:23:43 +0300

    feat: V-1-9-14.
    Исправлены ошибки:
    * Полуночное обновление

--- controllers/mission.py
+++ controllers/mission.py
@@ -79,7 +79,7 @@ class MissionController:
                 Fore.YELLOW + f"[{datetime.now()}][#]>>-||--> " +
                 Fore.GREEN + "Next mission in midnight"
             )
-            schedule.every(1).day.at("00:00").do(self.update, self).tag("send_mission")
+            schedule.every(1).day.at("00:00").do(self.update).tag("send_mission")
             return
 
         nearest = today_missions[0]
@@ -87,8 +87,9 @@ class MissionController:
             Fore.YELLOW + f"[{datetime.now()}][#]>>-||--> " +
             Fore.GREEN + f"Next mission at {nearest.send_time}"
         )
-        schedule.every(1).day.at(f"{nearest.send_time.hour}:{nearest.send_time.minute}").do(self.send, tuple(map(lambda t: t.operation[0], filter(
-            lambda x: x.send_time == nearest.send_time, today_missions
+        schedule.every(1).day.at(f"{nearest.send_time.hour}:{nearest.send_time.minute}").do(
+            self.send, tuple(map(lambda t: t.operation[0], filter(
+                lambda x: x.send_time == nearest.send_time, today_missions
         )))).tag("send_mission")
 
         if len(schedule.default_scheduler.jobs) == 1:
